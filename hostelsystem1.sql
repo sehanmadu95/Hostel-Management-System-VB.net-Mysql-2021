b0638d48e0772ea407d54cb76e0cea551a86a806
Sehan Madushanka <43729924+sehanmadu95@users.noreply.github.com>
2022-10-18 13:53:59 +0530

    Add files via upload

---
+++ hostelsystem1.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 18, 2019 at 08:47 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hostelsystem1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `loging`
+--
+
+CREATE TABLE `loging` (
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `hostel_Name` varchar(30) NOT NULL,
+  `q1` varchar(30) NOT NULL,
+  `q2` varchar(30) NOT NULL,
+  `q3` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
